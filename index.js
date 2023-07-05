b55a723e78a877d21bf73e9faf477239405e6150
samy fadel <samyfadel@google.com>
2023-07-05 16:48:57 +0200

    samy

--- index.js
+++ index.js
@@ -29,10 +29,13 @@ async function retrieveSmartContractABI(web3, blockNumber) {
     const block = await web3.eth.getBlock(blockNumber);
     const contractABIs = [];
 
+    console.log(block);
+
     for (const txHash of block.transactions) {
       const tx = await web3.eth.getTransaction(txHash);
       if (tx.to) {
         const code = await web3.eth.getCode(tx.to);
+        console.log(code);
         if (code.startsWith('0x') && code !== '0x') { // Add this check
           const contract = new web3.eth.Contract(JSON.parse(code), tx.to);
           const contractABI = contract.options.jsonInterface;
